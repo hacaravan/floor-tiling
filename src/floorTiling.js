21611b4c2f48f75ecb1b7589c4644b955e69c137
Hugh Cavanagh <hacavanagh@gmail.com>
2021-05-06 17:01:51 +0100

    calculateTileCoverage returns object with percentageWaste of 0

--- src/floorTiling.js
+++ src/floorTiling.js
@@ -1,7 +1,10 @@
 'use strict'
 
 function calculateTileCoverage() {
-  return { tileCount: 1 }
+  return {
+    tileCount: 1,
+    percentageWaste: 0 
+  }
 }
 
 module.exports = calculateTileCoverage
--- test/floorTiling.test.js
+++ test/floorTiling.test.js
@@ -4,8 +4,12 @@ const calculateTileCoverage = require('../src/floorTiling.js')
 
 describe('calculateTileCoverage', () => {
   describe('when passsed a tile size the same as the floor size', () => {
+    let result = calculateTileCoverage(10, 10, 10, 10);
     test('Returns an object with tileCount of 1', () => {
-      expect(calculateTileCoverage(10, 10, 10, 10).tileCount).toBe(1)
+      expect(result.tileCount).toBe(1)
+    })
+    test('Returns an object with percentageWaste of 0', () => {
+      expect(result.percentageWaste).toBe(0)
     })
   })
 })
