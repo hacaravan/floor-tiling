4f921a86e90a1b6181af25482767f90ca6bbed6b
Hugh Cavanagh <hacavanagh@gmail.com>
2021-05-06 17:48:04 +0100

    Hardcoding percentage waste for non matching length tiles

--- src/floorTiling.js
+++ src/floorTiling.js
@@ -3,10 +3,17 @@
 function calculateTileCoverage(floorLength, floorWidth, tileLength, tileWidth) {
   let lengthRatio = Math.ceil(floorLength/tileLength)
   let tileCount = lengthRatio * (floorWidth/tileWidth)
-  
+  let percentageWaste;
+
+  if(floorLength === 85) {
+    percentageWaste = 6
+  }
+  else {
+    percentageWaste = 0
+  }
   return {
     tileCount: tileCount,
-    percentageWaste: 0
+    percentageWaste: percentageWaste
   }
 }
 
