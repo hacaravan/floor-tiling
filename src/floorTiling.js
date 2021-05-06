7633acbfbf590642e298d06ef86462c2b9d05794
Hugh Cavanagh <hacavanagh@gmail.com>
2021-05-06 17:27:03 +0100

    Refactor: number of tiles used for length is based on ceiling of ratio

--- src/floorTiling.js
+++ src/floorTiling.js
@@ -1,13 +1,9 @@
 'use strict'
 
 function calculateTileCoverage(floorLength, floorWidth, tileLength, tileWidth) {
-  let tileCount
-  if(floorLength % tileLength !== 0){
-    tileCount = 45
-  } else {
-    tileCount = (floorLength/tileLength) * (floorWidth/tileWidth);
-  }
-
+  let lengthRatio = Math.ceil(floorLength/tileLength)
+  let tileCount = lengthRatio * (floorWidth/tileWidth)
+  
   return {
     tileCount: tileCount,
     percentageWaste: 0
